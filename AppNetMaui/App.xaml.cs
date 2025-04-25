93017edbd2b4b176ded0851040fcb5677ce215c3
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-04-25 10:55:51 -0300

    agregamos proyectos de Blazor y Net Maui

---
+++ AppNetMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppNetMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
