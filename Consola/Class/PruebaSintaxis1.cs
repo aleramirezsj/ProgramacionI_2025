f3405544706bbd165a77f2adeee4b433c270d001
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-05-13 14:16:46 -0300

    Añadir clases Calculadora y PruebaSintaxis1
    Se ha implementado la clase `Calculadora` con métodos para realizar operaciones matemáticas básicas y un método para imprimir resultados. También se ha creado la clase `PruebaSintaxis1` para imprimir saludos y deudas de clientes. En `Program.cs`, se han comentado las líneas de salida inicial y se ha añadido la creación de un objeto `Calculadora` para realizar una suma.

---
+++ Consola/Class/PruebaSintaxis1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola.Class
+{
+    public class PruebaSintaxis1
+    {
+        //Declaración de campos o propiedades
+        private double saldoFactura;
+        private string cliente;
+
+        //método que se llama igual que la clase y que no tiene tipo de retorno
+        //es el constructor de la clase
+        //se ejecuta cuando se crea un objeto de la clase
+        public PruebaSintaxis1()
+        {
+            saldoFactura = 1000.50;
+            cliente = "Juan Perez";
+            int numero = 10;
+            string nombre = "Juan";
+            double precio = 19.99;
+            bool esVerdadero = true;
+            //Declaramos y asignamos variables de tipo var
+            var edad = 25;
+            var apellido = "Ramirez";
+            var casado = false;
+        }
+        
+        public void ImprimirDeudaFactura()
+        {
+            Console.WriteLine(cliente + " tiene una deuda de " + saldoFactura);
+        }
+
+        public void ImprimirSaludo(string nom)
+        {
+            Console.WriteLine("Hola " + nom);
+        }
+    }
+}
--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,9 +1,15 @@
-﻿internal class Program
+﻿
+using Consola.Class;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
-
+        //var prueba= new PruebaSintaxis1();
+        //prueba.ImprimirSaludo("Alejandro Ramirez");
+        //prueba.ImprimirDeudaFactura();
+        var calcu = new Calculadora();
+        calcu.Sumar(10, 20);
+        calcu.ImprimirResultado();
     }
 }
